b05bde7f4e9884b7abca5ada3ba9949b31f5d147
vvkin <vkincur@gmail.com>
2020-02-10 16:45:40 +0200

    added comments

--- LAB_1/oop_source.py
+++ LAB_1/oop_source.py
@@ -2,7 +2,7 @@ from copy import copy
 
 
 class Graph:
-    def __init__(self, n, m, edges_list):
+    def __init__(self, n, m, edges_list):  # Constructor of graph
         self._n = n
         self._m = m
         self._edges_list = copy(edges_list)
@@ -79,7 +79,7 @@ class NotDirectedGraph(Graph):
         vertex_degrees = self.__get_vertex_degrees()
         return vertex_degrees[0]
 
-    def __enter_number(self):
+    def __enter_number(self):  # Enter and check number
         answer = 0
         while answer > 5 or answer < 1:
             answer = int(input("Type your choice : "))
@@ -87,7 +87,7 @@ class NotDirectedGraph(Graph):
                 print("Invalid input!")
         return answer
 
-    def __work_with_number(self, number):
+    def __work_with_number(self, number):  # Process number
         if number == 1:
             print_matrix(self.__get_incidence_matrix())
         elif number == 2:
@@ -103,7 +103,7 @@ class NotDirectedGraph(Graph):
             else:
                 print("Graph is not regular!")
 
-    def __show_menu(self):
+    def __show_menu(self):  # Show menu
         print("Choose one of the following : ")
         print("""
              1 - show incidence matrix
@@ -112,7 +112,7 @@ class NotDirectedGraph(Graph):
              4 - show hanging and isolated vertices
              5 - check graph for regulating  """)
 
-    def start_menu(self):
+    def start_menu(self):  # Start work with graph
         self.__show_menu()
         self.__work_with_number(self.__enter_number())
 
@@ -125,7 +125,7 @@ class DirectedGraph(Graph):
             adj_matrix[start - 1][finish - 1] = 1
         return adj_matrix
 
-    def __get_incidence_matrix(self):
+    def __get_incidence_matrix(self):  # Return incidence matrix
         inc_matrix = [[0] * len(self._edges_list) for j in range(self._n)]
         edge_number = 0
         for edge in self._edges_list:
@@ -138,26 +138,26 @@ class DirectedGraph(Graph):
             edge_number += 1
         return inc_matrix
 
-    def __get_half_in_degrees(self):
+    def __get_half_in_degrees(self):  # Get in-degree of each vertex
         half_in_degrees = [0] * self._n
         for start, finish in self._edges_list:
             half_in_degrees[finish - 1] += 1
         return half_in_degrees
 
-    def __get_half_out_degrees(self):
+    def __get_half_out_degrees(self):  # Get out-degree of each vertex
         half_out_degrees = [0] * self._n
         for start, finish in self._edges_list:
             half_out_degrees[start - 1] += 1
         return half_out_degrees
 
-    def __show_menu(self):
+    def __show_menu(self):  # Show menu
         print("Choose one of the following : ")
         print("""
                     1 - show incidence matrix
                     2 - show adjacency matrix
-                    3 - show half-in and half-out degrees """)
+                    3 - show in-degrees and out-degrees of each vertex """)
 
-    def __enter_number(self):
+    def __enter_number(self):  # Enter and check number
         answer = 0
         while answer > 3 or answer < 1:
             answer = int(input("Type your choice : "))
@@ -165,7 +165,7 @@ class DirectedGraph(Graph):
                 print("Invalid input!")
         return answer
 
-    def __work_with_number(self, number):
+    def __work_with_number(self, number):  # Process number
         if number == 1:
             print_matrix(self.__get_incidence_matrix())
         elif number == 2:
@@ -174,16 +174,16 @@ class DirectedGraph(Graph):
             print("Half - in degrees :", *self.__get_half_in_degrees())
             print("Half - out degrees :", *self.__get_half_out_degrees())
 
-    def start_menu(self):
+    def start_menu(self):  # Start work with number
         self.__show_menu()
         self.__work_with_number(self.__enter_number())
 
 
-def parse_row(string):  # Фукнція, яка приводить вхідні дані до потрібного формату
+def parse_row(string):  # Parse row of input data
     return tuple(map(int, string.split()))
 
 
-def get_input(file_name):  # Зчитування вхідних даних з файлу
+def get_input(file_name):  # Get data from input file
     edges_list = []
     with open(file_name, "r") as input_file:
         vertex, edges = parse_row(input_file.readline())
@@ -194,14 +194,14 @@ def get_input(file_name):  # Зчитування вхідних даних з �
     return vertex, edges, edges_list
 
 
-def print_matrix(matrix):
+def print_matrix(matrix):  # Print matrix
     for row in matrix:
         for i in row:
             print(i, end=" ")
         print()
 
 
-def main():
+def main():  # Start of program
     vertices_amount, edges_amount, edges_list = get_input("input.txt")
     while 1:
         type_of_graph = input("Is graph directed?(y/n) : ")
