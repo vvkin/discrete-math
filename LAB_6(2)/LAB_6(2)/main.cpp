88a584f7bd43ba8b8e757755ba7dc7ac2ee48993
Vadym Kinchur <vkincur@gmail.com>
2020-04-17 16:25:15 +0300

    the same algorithm but with adjacency list

--- LAB_6(2)/LAB_6(2)/main.cpp
+++ LAB_6(2)/LAB_6(2)/main.cpp
@@ -1,90 +1,79 @@
 #include <iostream>
 #include <fstream>
-#include <numeric>
 #include <list>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+void get_input(const char*, list<int>*&, int&);
+
 template<typename T>
-T** fill_matrix(const int, const int, const T);
-template<typename T>
-void delete_matrix(T**, const int);
+T* fill_array(const T, const int);
 
 int get_degree(int*, const int);
-int* get_degrees(int**, const int);
-void get_input(const char*, int**&, int&);
-vector<list<int>> get_colors(int**, const int);
-bool is_correct_color(int**,int*, const int, const int, const int);
-int* get_vertex_order(int**, int*, const int);
+int* get_degrees(list<int>*, const int);
+vector<list<int>> get_colors(list<int>*, const int);
+bool is_correct_color(list<int>*, int*, const int, const int, const int);
+int* get_vertex_order(int*, const int);
 
-void print_colors(int**, const int);
 
+
+void print_colors(list<int>*, const int);
 
 int main() {
-	int** adj_matrix = nullptr;
 	int v_num;
-	get_input("input.txt", adj_matrix, v_num);
-	print_colors(adj_matrix, v_num);
-	delete_matrix(adj_matrix, v_num);
+	list<int>* adj_list = nullptr;
+	get_input("input.txt", adj_list, v_num);
+	print_colors(adj_list, v_num);
+	delete[] adj_list;
 }
 
 template<typename T>
-T** fill_matrix(const int row_size, const int col_size, const T filler) {
-	T** matrix = new T * [row_size];
-	for (auto i = 0; i < row_size; ++i) {
-		matrix[i] = new T[col_size];
-	}
-	for (auto i = 0; i < row_size; ++i) {
-		for (auto j = 0; j < col_size; ++j) {
-			matrix[i][j] = filler;
-		}
-	}
-	return matrix;
+T* fill_array(const T filler, const int size) {
+	T* arr = new T[size];
+	for (auto i = 0; i < size; ++i)
+		arr[i] = filler;
+	return arr;
 }
 
-template<typename T>
-void delete_matrix(T** matrix, const int row_size) {
-	for (auto i = 0; i < row_size; ++i) 
-		delete[] matrix[i];
-	delete[] matrix;
-}
-
-void get_input(const char* file_name, int**& matrix, int& v_num) {
+void get_input(const char* file_name, list<int>*& adj_list, int& v_num) {
 	ifstream file_handler(file_name);
 	int e_num;
 	file_handler >> v_num >> e_num;
-	matrix = fill_matrix(v_num, v_num, 0);
+	adj_list = new list<int>[v_num];
 
 	for (auto i = 0; i < e_num; ++i) {
 		int start, end;
 		file_handler >> start >> end;
-		matrix[start - 1][end - 1] += 1;
-		matrix[end - 1][start - 1] += 1;
+		adj_list[start - 1].push_back(end - 1);
+		adj_list[end - 1].push_back(start - 1);
 	}
 	file_handler.close();
 }
 
-bool is_correct_color(int** adj_m, int* colors, const int v, const int color, const int v_num) {
-	for (auto i = 0; i < v_num; ++i) {
-		if (adj_m[v][i] && colors[i] == color)
+bool is_correct_color(list<int>* adj_l, int* colors, const int v, const int color, const int v_num) {
+	for (auto vertex : adj_l[v]) {
+		if (colors[vertex] == color)
 			return false;
 	}
 	return true;
 }
 
-int* get_degrees(int** adj_m, const int size) {
-	int* degrees = new int[size];
-	for (auto i = 0; i < size; ++i) {
-		degrees[i] = accumulate(adj_m[i], adj_m[i] + size, 0);
+int* get_degrees(list<int>* adj_l, const int v_num) {
+	int* degrees = new int[v_num];
+	for (auto v = 0; v < v_num; ++v) {
+		degrees[v] = adj_l[v].size();
 	}
 	return degrees;
 }
 
-int* get_vertex_order(int** adj_m, int* degrees, const int v_num) {
-	int* order = new int[v_num];
+int get_degree(int* deg, const int v_num) {
+	return *max_element(deg, deg + v_num) + 1;
+}
 
-	for (auto i = 0; i < v_num; ++i) 
+int* get_vertex_order(int* degrees, const int v_num) {
+	int* order = new int[v_num];
+	for (auto i = 0; i < v_num; ++i)
 		order[i] = i;
 
 	for (auto i = 0; i < v_num; ++i) {
@@ -98,19 +87,16 @@ int* get_vertex_order(int** adj_m, int* degrees, const int v_num) {
 	return order;
 }
 
-vector<list<int>> get_colors(int** adj_m, const int v_num){
+vector<list<int>> get_colors(list<int>* adj_l, const int v_num) {
 	vector<list<int> > color_set;
-	auto* degrees = get_degrees(adj_m, v_num);
+	auto* degrees = get_degrees(adj_l, v_num);
 	const auto max_colors = get_degree(degrees, v_num);
-	int* colors = new int[v_num];
-	auto* order = get_vertex_order(adj_m, degrees, v_num);
-	
-	for (auto i = 0; i < v_num; ++i)
-		colors[i] = -1;
+	auto* colors = fill_array(-1, v_num);
+	auto* order = get_vertex_order(degrees, v_num);
 
 	for (auto i = 0; i < v_num; ++i) {
 		for (auto color = 0; color < max_colors; ++color) {
-			if (is_correct_color(adj_m, colors, order[i], color, v_num)) {
+			if (is_correct_color(adj_l, colors, order[i], color, v_num)) {
 				if (color >= color_set.size())
 					color_set.push_back(list<int>{});
 				colors[order[i]] = color;
@@ -125,13 +111,8 @@ vector<list<int>> get_colors(int** adj_m, const int v_num){
 	return color_set;
 }
 
-
-int get_degree(int* deg, const int v_num) {
-	return *max_element(deg, deg + v_num) + 1;
-}
-
-void print_colors(int** adj_m, const int v_num) {
-	auto color_set = get_colors(adj_m, v_num);
+void print_colors(list<int>* adj_l, const int v_num) {
+	auto color_set = get_colors(adj_l, v_num);
 	for (auto i = 0; i < color_set.size(); ++i) {
 		cout << "Color " << i + 1 << '\n';
 		for (auto el : color_set[i]) {
