53bfd8fe75650bf5d091d4bac1bb0baa7a2ba139
vvkin <vkincur@gmail.com>
2020-05-19 20:51:45 +0300

    finished 8(1) (Maximal flow)

--- LAB_8(1)/LAB_8(1)/Program.cs
+++ LAB_8(1)/LAB_8(1)/Program.cs
@@ -1,43 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace LAB_8_1_
 {
-    class Edge
-    {
-        public int dest, flow, pred;
-        public Edge(int dest, int flow, int pred = -1)
-        {
-            this.dest = dest;
-            this.flow = flow;
-            this.pred = pred;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var adjL = GetInput("input.txt");
+            var adjM = GetInput("input.txt");
+            (var maxFlow, var newAdj) = ProcessFulkerson(adjM);
+
+            PrintFulkerson(maxFlow, newAdj, adjM);
         }
 
-        static Dictionary<int, List<Edge>> GetInput(string fileName)
+        static int[,] GetInput(string fileName)
         {
             string path = "../../../" + fileName;
             using (var sr = new StreamReader(path))
             {
                 var fisrstRow = sr.ReadLine().Split().ToList().ConvertAll(s => int.Parse(s));
                 (var vNum, var eNum) = (fisrstRow[0], fisrstRow[1]);
-                var adjL = new Dictionary<int, List<Edge>>(vNum);
-                for (var i = 0; i < vNum; ++i) adjL[i] = new List<Edge>();
-
+                var adjM = new int[vNum, vNum];
                 for(var i = 0; i < eNum; ++i)
                 {
                     (var start, var end, var flow) = ParseRow(sr.ReadLine());
-                    adjL[start - 1].Add(new Edge(end - 1, flow));
+                    adjM[start - 1, end - 1] = flow;
                 }
-                return adjL;
+                return adjM;
             }
         }
 
@@ -47,31 +37,98 @@ namespace LAB_8_1_
             return (nums[0], nums[1], nums[2]);
         }
 
-
-        static (int, int) GetSourceAndSick(Dictionary<int, List<Edge>> adjL)
+        static (int, int) GetSourceAndSick(int[,] adjM)
         {
-            var inDegree = new int[adjL.Count];
-            var outDegree = new int[adjL.Count];
-            for(var i = 0; i < adjL.Count; ++i)
+            var size = adjM.GetLength(0);
+            var inDegree = new int[size];
+            var outDegree = new int[size];
+            (var sick, var source) = (0, 0);
+            for(var i = 0; i < size; ++i)
             {
-                outDegree[i] = adjL[i].Sum(s => s.flow);
-                foreach(var edge in adjL[i])
+                for(var j = 0; j < size; ++j)
                 {
-                    
+                    if (adjM[i, j] != 0)
+                    {
+                        ++outDegree[i];
+                        ++inDegree[j];
+                    }
+                    source = (inDegree[j] == 0) ? j : (inDegree[i] == 0) ? i : source;
+                    sick = (outDegree[i] == 0) ? i : (outDegree[j] == 0) ? j : sick;
+                }
+            }
+            return (source, sick);
+        }
+
+        static bool BFS(int[,] adjM, int source, int sick, ref int[] parent)
+        {
+            var visited = Enumerable.Repeat(false, adjM.Length).ToArray();
+            var queue = new Queue<int>();
+            visited[source] = true; queue.Enqueue(source);
+
+            while(queue.Count != 0)
+            {
+                var u = queue.Dequeue();
+                for(var v = 0; v < adjM.GetLength(0); ++v)
+                {
+                    if (!visited[v] && adjM[u, v] > 0)
+                    {
+                        queue.Enqueue(v);
+                        visited[v] = true;
+                        parent[v] = u;
+                    }
+                }
+            }
+            return (visited[sick]);
+        }
+
+        static (int, int[,]) ProcessFulkerson(int[,] adjM)
+        {
+            (var source, var sick) = GetSourceAndSick(adjM);
+            var parent = Enumerable.Repeat(-1, adjM.Length).ToArray();
+            var changeAdjM = adjM.Clone() as int[,];
+            var maxFlow = 0;
+            
+            while(BFS(changeAdjM, source, sick, ref parent))
+            {
+                var currentFlow = int.MaxValue;
+                var start = sick;
+
+                while(start != source)
+                {
+                    currentFlow = System.Math.Min(currentFlow, changeAdjM[parent[start], start]);
+                    start = parent[start];
+                }
+
+                maxFlow += currentFlow;
+                var v = sick;
+
+                while(v != source)
+                {
+                    var u = parent[v];
+                    changeAdjM[u, v] -= currentFlow;
+                    changeAdjM[v, u] += currentFlow;
+                    v = parent[v];
+                }
+            }
+            return (maxFlow, changeAdjM);
+        }
+
+        static void PrintFulkerson(int maxFlow, int[,]  newadjM, int [,] adjM)
+        {
+            System.Console.WriteLine($"Maximal flow is equal to {maxFlow}");
+            System.Console.WriteLine("Flow through each edge: \n");
+            for(var i = 0; i < adjM.GetLength(0); ++i)
+            {
+                for(var j = 0; j < adjM.GetLength(0); ++j)
+                {
+                    if (adjM[i, j] != 0)
+                    {
+                        var flow = (newadjM[i, j] < 0) ? adjM[i, j]
+                                                       : adjM[i, j] - newadjM[i, j];
+                        System.Console.WriteLine($"({i + 1}, {j + 1}) : {flow}");
+                    }
                 }
             }
         }
-
-        static (int[], int[]) GetDegrees(Dictionary<int, List<Edge>>)
-        {
-
-        }
-
-
-        static void ProcessFulkerson(Dictionary<int, List<Edge>> startFlows)
-        {
-            var adjL = new Dictionary<int, List<Edge>>(startFlows);
-        }
-
     }
 }
---
+++ Solutions in one file/DSIP91LR7(2)KINCHURVADYM.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <iomanip>
+using namespace std;
+
+struct two_parts {
+	vector<int> first, second;
+};
+
+void get_input(const char*, list<int>*&, int&);
+vector<int> get_bipartition(list<int>*, int);
+two_parts get_two_parts(vector<int>);
+vector<int*> get_matches(list<int>*, vector<int>);
+void find_matches(vector<int*>&, list<int>*, vector<int>&, int&, int);
+char** get_template_matrix(list<int>*, two_parts);
+void print_match(int*, vector<int>, vector<int>, char**);
+void print_matrix(char**, int, int, vector<int>);
+void print_matches(list<int>*,int);
+void print_vector(vector<int>);
+
+int main() {
+	list<int>* adj_list = nullptr;
+	int v_num;
+	get_input("input.txt", adj_list, v_num);
+	print_matches(adj_list, v_num);
+}
+
+void get_input(const char* file_name, list<int>*& adj_l, int& v_num) {
+	ifstream file_handler(file_name);
+	int e_num, start, end;
+	file_handler >> v_num >> e_num;
+	adj_l = new list<int>[v_num];
+
+	for (auto i = 0; i < e_num; ++i) {
+		file_handler >> start >> end;
+		adj_l[start - 1].push_back(end - 1);
+		adj_l[end - 1].push_back(start - 1);
+	}
+	file_handler.close();
+}
+
+vector<int> get_bipartition(list<int>* adj_l, int v_num) {
+	vector<int> bipart(v_num, -1);
+	queue<int> que;
+	for (auto i = 0; i < v_num; ++i) {
+		if (bipart[i] != -1 || adj_l[i].empty()) continue;
+		que.push(i); bipart[i] = 0;
+		while (!que.empty()) {
+			int start = que.front();
+			que.pop();
+			for (auto v : adj_l[start]) {
+				if (bipart[v] == -1) {
+					bipart[v] = 1 - bipart[start];
+					que.push(v);
+				}
+				else if (bipart[v] == bipart[start]) {
+					return vector<int>();
+				}
+			}
+
+		}
+	}
+	return bipart;
+}
+
+two_parts get_two_parts(vector<int> bipart) {
+	two_parts res;
+	for (auto i = 0; i < bipart.size(); ++i) {
+		if (bipart[i]) res.first.push_back(i);
+		else res.second.push_back(i);
+	}
+	if (res.second.size() < res.first.size())
+		swap(res.second, res.first);
+	return res;
+}
+
+void find_matches(vector<int*>& matching, list<int>* adj_l, vector<int>& l_part, int& to, int at) {
+	for (auto& v : adj_l[l_part[at]]) {
+		auto finded = false;
+		for (auto i = 0; i < at; ++i) {
+			if (matching[to][i] == v) {
+				finded = true; break;
+			}
+		}
+		if (finded) continue;
+		matching[to][at] = v;
+		if (at == l_part.size() - 1) {
+			matching.push_back(new int[l_part.size()]);
+			for (auto i = 0; i < l_part.size() - 1; ++i) {
+				matching[to + 1][i] = matching[to][i];
+			}
+			++to; return;
+		}
+		find_matches(matching, adj_l, l_part, to, at + 1);
+	}
+	if (!at) matching.pop_back();
+}
+
+vector<int*> get_matches(list<int>* adj_l, vector<int> l_part) {
+	vector<int*> matching{ new int[l_part.size()] };
+	int to = 0;
+	find_matches(matching, adj_l, l_part, to, 0);
+	return matching;
+}
+
+
+char** get_template_matrix(list<int>* adj_l, two_parts parts) {
+	auto** t_matrix = new char*[parts.first.size()];
+	for (auto i = 0; i < parts.first.size(); ++i) {
+		t_matrix[i] = new char[parts.second.size()];
+	}
+	for (auto i = 0; i < parts.first.size(); ++i) {
+		for (auto j = 0; j < parts.second.size(); ++j) {
+			auto el = find(adj_l[parts.first[i]].begin(), 
+				           adj_l[parts.first[i]].end(), parts.second[j]);
+			t_matrix[i][j] = (el != adj_l[parts.first[i]].end()) ? '0' : '*';
+		}
+	}
+	return t_matrix;
+}
+
+void print_matrix(char** matrix, int row_num, int col_num, vector<int> l_part) {
+	for (auto i = 0; i < row_num; ++i) {
+		cout << setw(4) << l_part[i] + 1;
+		for (auto j = 0; j < col_num; ++j) {
+			cout << setw(4) << matrix[i][j];
+			matrix[i][j] = (matrix[i][j] != '*') ? '0' : '*';
+		}
+		cout << endl;
+	}
+}
+
+void print_vector(vector<int> vec) {
+	cout << "    ";
+	for (auto el : vec) {
+		cout << setw(4) << el + 1;
+	}
+	cout << endl;
+}
+
+
+void print_match(int* match, vector<int> l_part, vector<int> r_part, char** t_matrix) {
+	print_vector(r_part);
+	for (auto i = 0; i < l_part.size(); ++i) {
+		auto index = distance(r_part.begin(), find(r_part.begin(), r_part.end(), match[i]));
+		t_matrix[i][index] = '1';
+	}
+	print_matrix(t_matrix, l_part.size(), r_part.size(), l_part);
+}
+
+void print_matches(list<int>* adj_l, int v_num) {
+	auto bipart = get_bipartition(adj_l, v_num);
+	if (bipart.size() == 0) {
+		cout << "Graph is not biparted\n";
+		return;
+	}
+	auto parts = get_two_parts(bipart);
+	auto** t_matrix = get_template_matrix(adj_l, parts);
+	auto matching = get_matches(adj_l, parts.first);
+	for (auto i = 0; i < matching.size(); ++i) {
+		cout << "Matching #" << i + 1 << ':' << endl;
+		print_match(matching[i], parts.first, parts.second, t_matrix);
+		cout << endl;
+	}
+}
